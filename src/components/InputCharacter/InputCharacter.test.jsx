601bb1a52d31c66f150133095202041abda56cc6
Renan Pina <renan.pina@hp.com>
2023-02-27 10:15:03 -0300

    Adding object to ma the braille points and updating unit tests

--- src/components/InputCharacter/InputCharacter.test.jsx
+++ src/components/InputCharacter/InputCharacter.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import InputCharacter from './InputCharacter'
 import userEvent from '@testing-library/user-event'
 
@@ -13,6 +13,6 @@ test('Enters a character and check result', () => {
   render(<InputCharacter />)
   const inputElement = screen.getByRole('textbox', { name: /character/ })
   userEvent.type(inputElement, 'x')
-  const regionElement = screen.getByText(/x/)
+  const regionElement = screen.getByText(/1, 3, 4, 6/)
   expect(regionElement).toBeInTheDocument()
 })
--- src/components/InputCharacter/InputCharacter.tsx
+++ src/components/InputCharacter/InputCharacter.tsx
@@ -1,24 +1,21 @@
 import React, { useState } from 'react'
+import brailleMap from '../../brailleMap'
 
 const InputCharacter = () => {
   const [dots, setDots] = useState('')
   const handleOnChange = (event: any) => {
+    const character = event.target.value
     event.target.select()
-    setDots(event.target.value)
+    setDots(brailleMap[character])
   }
 
   return (
     <>
       <label htmlFor="character">Input character: </label>
-      <input
-        type="text"
-        id="character"
-        name="character"
-        autoFocus
-        maxLength={1}
-        onChange={handleOnChange}
-      />
-      <p role='region' aria-live='polite'>{dots}</p>
+      <input type="text" id="character" name="character" autoFocus maxLength={1} onChange={handleOnChange} />
+      <p role="region" aria-live="polite">
+        {dots}
+      </p>
     </>
   )
 }
